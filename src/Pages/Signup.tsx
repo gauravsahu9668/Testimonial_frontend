163f6fb7df11bfb23675dac80298389efd25248e
gauravsahu9668 <sahug6194@gmail.com>
2024-12-23 14:24:10 +0530

    addedotp

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -1,10 +1,12 @@
 import {useForm} from "react-hook-form"
 import { Link, useNavigate } from "react-router-dom"
 import google from '../assets/google.png'
-import axiosConnect from "../server/axiosConnect"
+import axiosConnect, { BACKEND_URL } from "../server/axiosConnect"
+import axios from "axios"
 import toast from "react-hot-toast"
 import { createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import { app } from '../server/firebase'
+import { useState } from "react"
 const auth=getAuth(app)
 const Signup = () => {
     type FormValues = {
@@ -16,134 +18,95 @@ const Signup = () => {
         register,reset,handleSubmit,
         formState:{errors}
     }=useForm<FormValues>()
+    const [sendotp,setsendotp]=useState(false);
+    const [useremail,setemail]=useState("");
+    const [userpassword,setpassword]=useState("");
+    const [firstname,setfirstname]=useState("");
+    const [generatedotp,setgeneratedotp]=useState("");
+    const [userotp, setOtp] = useState("");
     const navigator=useNavigate();
+    const handleChange = (e:any) => {
+      const value = e.target.value;
+      // Allow only numeric values and limit to 6 digits
+      if (/^\d{0,6}$/.test(value)) {
+        setOtp(value);
+      }
+    };
     const submitFormHandler=async(data:FormValues)=>{
-        createUserWithEmailAndPassword(auth,data.email,data.password).then((response)=>{
+    console.log(data)
+    try{
+      const toastId=toast.loading("Loading....");
+      await axios({
+        url:`${BACKEND_URL}/user/send-otp`,
+        method:"POST",
+        data:{
+          email:data.email
+        }
+      }).then((res:any)=>{
+        toast.dismiss(toastId)
+          setgeneratedotp(res.data.otp);
+          toast.success(res.data.message)
+      })
+    }catch(e){
+      console.log(e);
+    }
+    setsendotp(true);
+    setemail(data.email);
+    setfirstname(data.firstname)
+    setpassword(data.password);
+    }
+    const submitsignup=async()=>{
+      const toastId=toast.loading("Loading...");
+      if(generatedotp===userotp){
+        createUserWithEmailAndPassword(auth,useremail,userpassword).then((response)=>{
           if(response){
             try{
-              axiosConnect("/user/signup","POST",{firstName:data.firstname,email:data.email,password:data.password},"").then((result)=>{
+              axiosConnect("/user/signup","POST",{firstName:firstname,email:useremail,password:userpassword},"").then((result)=>{
                   console.log(result?.data)
+                  toast.dismiss(toastId)
                   toast.success("Account has been created")
                   navigator("/signin")
             }) 
           }catch(e){
               console.log("error while signing up")
           }
-          reset();
            }
         }).catch((error)=>{
           console.log(error)
           alert(error)
         })
+      }
+      else{
+        toast.dismiss(toastId);
+        toast.error("Please signup after some time")
+      }
     }
   return (
-  //   <div className="w-full min-h-[100vh] flex flex-col bg-[#EEF3F6]">
-  // <div className="w-full text-center text-[#151719] mt-8 md:mt-16 font-bold text-[28px] md:text-[35px]">
-  //   Sign up for free
-  // </div>
-  // <div className="w-full text-center text-[18px] md:text-[22px] text-[#53575C]">
-  //   You will get 2 video and 10 text testimonial credits for FREE!
-  // </div>
-  // <div className="w-[90%] md:w-[60%] lg:w-[40%] xl:w-[32%] mx-auto p-5 mb-10 bg-white mt-10 md:mt-16">
-  //   <div className="flex gap-x-2">
-  //     <input
-  //       className="w-[20px] md:w-[25px] focus:border-blue-500"
-  //       type="checkbox"
-  //       id="check-box"
-  //     ></input>
-  //     <div className="text-[14px] md:text-[16px] text-[#78909C]">
-  //       I agree to the Testimonial Terms of Service and I'm aware my personal
-  //       data is processed in accordance with our Privacy Policy. Please read
-  //       it carefully.
-  //     </div>
-  //   </div>
-  //   <div className="w-full mt-5 cursor-pointer p-3 bg-[#F5F8FA] border-[1px] rounded-sm flex flex-row">
-  //     <div className="border-r-[1px] rounded-sm flex items-center justify-center w-[15%]">
-  //       <img src={google} className="w-[18px] md:w-[23px]"></img>
-  //     </div>
-  //     <div className="w-full text-center text-[16px] md:text-[20px] text-[#78909C]">
-  //       Sign in with Google
-  //     </div>
-  //   </div>
-  //   <div className="flex mt-5 flex-row items-center justify-between text-center text-[14px] md:text-[16px] text-[#78909C]">
-  //     <div className="h-[1px] w-[20%] md:w-[25%] bg-gray-500"></div>
-  //     <p>Or, sign in with your email</p>
-  //     <div className="h-[1px] w-[20%] md:w-[25%] bg-gray-500"></div>
-  //   </div>
-  //   <div>
-  //     <form onSubmit={handleSubmit(submitFormHandler)}>
-  //       <div className="mt-6 flex flex-col items-start gap-y-1">
-  //         <label
-  //           htmlFor="firstname"
-  //           className="text-[14px] md:text-[16px] text-[#78909C]"
-  //         >
-  //           First Name
-  //         </label>
-  //         <input
-  //           id="firstname"
-  //           {...register("firstname")}
-  //           placeholder="Enter first name"
-  //           className="outline-none w-full p-2 text-[16px] md:text-[20px] focus:border-2 focus:border-blue-500 text-[#78909C] border-[1px] border-gray-600"
-  //         ></input>
-  //         {errors.password && <span>Your first name</span>}
-  //       </div>
-  //       <div className="mt-3 flex flex-col items-start gap-y-1">
-  //         <label
-  //           htmlFor="email"
-  //           className="text-[14px] md:text-[16px] text-[#78909C]"
-  //         >
-  //           Email
-  //         </label>
-  //         <input
-  //           id="email"
-  //           {...register("email")}
-  //           type="email"
-  //           placeholder="Your email"
-  //           className="outline-none w-full p-2 text-[16px] md:text-[20px] focus:border-2 focus:border-blue-500 text-[#78909C] border-[1px] border-gray-600"
-  //         ></input>
-  //         {errors.email && <span>Enter email</span>}
-  //       </div>
-  //       <div className="mt-3 flex flex-col items-start gap-y-1">
-  //         <label
-  //           htmlFor="password"
-  //           className="text-[14px] md:text-[16px] text-[#78909C]"
-  //         >
-  //           Password
-  //         </label>
-  //         <input
-  //           id="password"
-  //           {...register("password")}
-  //           type="password"
-  //           placeholder="Enter password"
-  //           className="outline-none w-full p-2 text-[16px] md:text-[20px] focus:border-2 focus:border-blue-500 text-[#78909C] border-[1px] border-gray-600"
-  //         ></input>
-  //         {errors.password && <span>Enter password</span>}
-  //       </div>
-  //       <p className="mt-2 text-[14px] md:text-[16px] text-[#78909C]">
-  //         Forgot password?
-  //       </p>
-  //       <button
-  //         type="submit"
-  //         className="text-white mt-6 rounded-sm font-semibold text-[14px] md:text-[16px] w-full py-2 bg-[#5D5DFF] hover:bg-[#4b4bfb]"
-  //       >
-  //         Sign up
-  //       </button>
-  //     </form>
-  //     <div className="w-full flex items-center gap-x-1 justify-center flex-row mt-4">
-  //       <p className="text-[14px] md:text-[16px] text-[#757D86]">
-  //         Already have an account?
-  //       </p>
-  //       <Link
-  //         to="/signin"
-  //         className="text-[14px] md:text-[16px] text-[#5377c3] hover:text-[#738cc1]"
-  //       >
-  //         Log in
-  //       </Link>
-  //     </div>
-  //   </div>
-  // </div>
-  //   </div>
-  <div className="w-full min-h-[100vh] flex flex-col bg-[#F9FAFB]">
+  <>{
+    sendotp ?
+    <div className="flex justify-center items-center mt-3 py-16 bg-gray-100">
+    <div className="bg-white p-6 rounded-lg shadow-lg">
+      <h1 className="text-2xl font-semibold text-gray-800 text-center mb-4">Enter OTP</h1>
+      <form onSubmit={handleSubmit(submitsignup)} className="flex flex-col space-y-4">
+        <input
+          type="text"
+          value={userotp}
+          onChange={handleChange}
+          maxLength={6}
+          placeholder="Enter 6-digit OTP"
+          className="text-center text-lg px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 hover:border-indigo-300 transition-all duration-300"
+        />
+        <button
+          type="submit"
+          className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600 focus:ring-4 focus:ring-indigo-300 transition-all duration-300"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+    </div>
+    :
+    <div className="w-full min-h-[100vh] flex flex-col bg-[#F9FAFB]">
   <div className="w-full text-center text-[#212529] mt-8 md:mt-16 font-semibold text-[28px] md:text-[35px]">
     Sign up for free
   </div>
@@ -258,7 +221,9 @@ const Signup = () => {
       </Link>
     </div>
   </div>
-</div>
+  </div>
+  }
+  </>
 
 
 
